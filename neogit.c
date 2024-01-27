ca71fe4b2a6e8fc86def3d5a8c0b9743b2e5e8eb
Omid <iamomidshafaat@gmail.com>
2024-01-27 18:38:46 +0330

    init completed

---
+++ neogit.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+void run_init(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+
+    if (argc < 2)
+    {
+        fprintf(stdout, "please enter a valid command");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "init") == 0)
+    {
+        run_init(argc, argv);
+    }
+
+    return 0;
+}
+void run_init(int argc, char *argv[])
+{
+    char firstDirectory[FILENAME_MAX];
+    getcwd(firstDirectory, sizeof(firstDirectory));
+
+    struct dirent *entry;
+    struct stat file_info;
+
+    int neogit_exists = 0;
+    char currentDirectory[FILENAME_MAX];
+    do
+    {
+        getcwd(currentDirectory, sizeof(currentDirectory));
+        DIR *dir = opendir(".");
+        while ((entry = readdir(dir)) != NULL)
+        {
+            stat(entry->d_name, &file_info);
+            if (S_ISDIR(file_info.st_mode) && strcmp(entry->d_name, ".neogit") == 0)
+            {
+                printf("exists\n");
+                neogit_exists = 1;
+                break;
+            }
+        }
+        chdir("..");
+    } while (neogit_exists != 1 && strcmp(currentDirectory, "C:\\") != 0);
+    chdir(firstDirectory);
+
+    if (neogit_exists == 0)
+    {
+        system("mkdir .neogit");
+        system("attrib +h .neogit");
+    }
+}
